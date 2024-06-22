ad43632737f4dcc5b9ceb7531aebde93dbb7e6c7
shgopher <shgopher@gmail.com>
2024-06-22 13:39:29 +0800

    feat(basic): add helloworld.cj

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # learn-cangjie-by-example
 Learn cangjie by example; 通过例子学习仓颉编程语言
+## 基础语法
+- [helloworld](./basic/helloworld.cj)
+## 并发
+## 工程
---
+++ lint.sh
@@ -0,0 +1,47 @@
+find_md(){
+###
+ # @Author: shgopher shgopher@gmail.com
+ # @Date: 2024-06-22 13:34:32
+ # @LastEditors: shgopher shgopher@gmail.com
+ # @LastEditTime: 2024-06-22 13:34:46
+ # @FilePath: /learn-cangjie-by-example/lint.sh
+ # @Description: 
+ # 
+ # Copyright (c) 2024 by shgopher, All Rights Reserved. 
+### 
+
+  local printed_files=()
+
+  # 检查 zhlint 命令是否存在  
+  if ! [ -x "$(command -v zhlint)" ]; then
+
+    # 安装 zhlint
+    npm install -g zhlint > /dev/null 
+  fi
+
+  for item in $(ls -R "$1"); do
+
+    if [[ "$item" == *.md ]]; then
+      
+      path="$1/$item"
+
+      if [[ ! " ${printed_files[@]} " =~ " $path " ]]; then
+      
+        echo "$path"
+
+        # 调用zhlint格式化    
+        zhlint "$path" --fix > /dev/null
+      
+        printed_files+=("$path")
+      fi
+
+    elif [[ -d "$1/$item" ]]; then
+
+      find_md "$1/$item" 
+    fi
+
+  done  
+}
+
+# 调用 
+find_md "$(pwd)"
